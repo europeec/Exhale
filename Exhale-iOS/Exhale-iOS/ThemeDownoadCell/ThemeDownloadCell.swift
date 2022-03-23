819f98eef1c81868493223d051ed46152bc51017
europeec <l.europeec@ya.ru>
2022-03-23 11:42:52 +0300

    litle bit refactoring

--- Exhale-iOS/Exhale-iOS/ThemeDownoadCell/ThemeDownloadCell.swift
+++ Exhale-iOS/Exhale-iOS/ThemeDownoadCell/ThemeDownloadCell.swift
@@ -5,7 +5,6 @@ final class ThemeDownloadCell: UITableViewCell {
     
     private var theme: Theme?
     private var indexPath: IndexPath?
-//    private weak var delegate: ThemeDownloadCellDelegate?
     
     var state: DownloadTaskState = .none
     
@@ -52,27 +51,5 @@ final class ThemeDownloadCell: UITableViewCell {
         case .none:
             progressBar.progress = 0
         }
-        
     }
-
 }
-//
-//extension ThemeDownloadCell: URLSessionDownloadDelegate {
-//    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-//        
-//        isDone = true
-//        guard let indexPath = indexPath else { return }
-//        
-//        delegate?.cell(at: indexPath, didUpdateState: .done(location: location))
-//    }
-//    
-//    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-//
-//        guard let indexPath = indexPath else { return }
-//
-//        let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
-//        progressBar.progress = progress
-//
-//        delegate?.cell(at: indexPath, didUpdateState: .loading(progress: progress))
-//    }
-//}
