58677eb60a6ae9bffe25637264653c20b803299c
Ivan Semerenko <vanya992008@gmail.com>
2022-03-28 23:17:00 +0300

    Create solution REST API

---
+++ WebApi/WebApiExhale/Models/UsersContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiExhale.Models
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
